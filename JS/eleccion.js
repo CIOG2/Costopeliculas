3c33591d118b1ab81f5fc41216e0c72f73d8dc20
Carlos Iván Ochoa Gómez <81680537+CIOG2@users.noreply.github.com>
2021-08-10 21:08:05 -0600

    Termine la funcion calcular

--- JS/eleccion.js
+++ JS/eleccion.js
@@ -1,5 +1,5 @@
 const listaDeCuentasPagadas = [];
-let nexflix = false, youtube = false, hboMax = false, disney = false, primeVideo = false, paramount = false, hulu = false, appleTv = false, plutoTv = false, blimTv = false, crunchyroll = false, claroVideo = false; 
+let nexflix = false, youtube = false, hboMax = true, disney = false, primeVideo = true, paramount = false, hulu = false, appleTv = false, plutoTv = false, blimTv = false, crunchyroll = false, claroVideo = false; 
 let precio = 0;
 
 function markedNexflix() {
@@ -10,15 +10,12 @@ function markedNexflix() {
         document.getElementById('nexflixRES1').checked = false;
         document.getElementById('nexflixRES2').checked = false;
         document.getElementById('nexflixRES3').checked = false;
-        
         nexflix = false;
     } else {
         document.getElementById('nexflix').classList.add('marked');
-
         setTimeout(() => {
             document.getElementById('nexflixPlanes').classList.add('display'); 
         }, 50); 
-
         nexflix = true;
     }    
 }
@@ -28,8 +25,7 @@ function markedHulu() {
         document.getElementById('hulu').classList.remove('marked');
         document.getElementById('huluRES1').checked = false;
         document.getElementById('huluRES2').checked = false;
-        document.getElementById('huluRES3').checked = false;
-        
+        document.getElementById('huluRES3').checked = false;        
         hulu = false;
     } else {
         document.getElementById('hulu').classList.add('marked');
@@ -58,10 +54,6 @@ function markedCrunchyroll() {
         crunchyroll = true;
     }    
 }
-
-
-
-
 
 function markedYoutube() {
     if(youtube){
@@ -145,40 +137,87 @@ function markedClaroVideo(plataforma) {
     } 
 }
 
-
 function removeDisplay(id) {        
     setTimeout(() => {
         document.getElementById(id).classList.remove('display');  
     }, 200);
 }
 
-
-
 function CALCULAR() {
-    let precio1 = document.getElementById('nexflixRES1').checked;
-    let precio2 = document.getElementById('nexflixRES2').checked;
-    let precio3 = document.getElementById('nexflixRES3').checked;
+    let NEprecio1 = document.getElementById('nexflixRES1').checked;
+    let NEprecio2 = document.getElementById('nexflixRES2').checked;
+    let NEprecio3 = document.getElementById('nexflixRES3').checked;
+    let HUprecio1 = document.getElementById('huluRES1').checked;
+    let HUprecio2 = document.getElementById('huluRES2').checked;
+    let HUprecio3 = document.getElementById('huluRES3').checked;
+    let CRprecio1 = document.getElementById('crunchyrollRES1').checked;
+    let CRprecio2 = document.getElementById('crunchyrollRES2').checked;
 
-    if(precio1){
+    if(NEprecio1){
         precio = 139;
         listaDeCuentasPagadas[listaDeCuentasPagadas.length] = precio;
     }
-    if(precio2){
-        precio = 196;
+        if(NEprecio2){
+            precio = 196;
+            listaDeCuentasPagadas[listaDeCuentasPagadas.length] = precio;
+        }
+            if(NEprecio3){
+                precio = 266;
+                listaDeCuentasPagadas[listaDeCuentasPagadas.length] = precio;
+            }
+                if(HUprecio1){
+                    precio = 139;
+                    listaDeCuentasPagadas[listaDeCuentasPagadas.length] = precio;
+                }  
+                    if(HUprecio2){
+                        precio = 196;
+                        listaDeCuentasPagadas[listaDeCuentasPagadas.length] = precio;
+                    }
+                        if(HUprecio3){
+                            precio = 266;
+                            listaDeCuentasPagadas[listaDeCuentasPagadas.length] = precio;
+                        }
+                            if(CRprecio1){
+                                precio = 139;
+                                listaDeCuentasPagadas[listaDeCuentasPagadas.length] = precio;
+                            }    
+                                if(CRprecio2){
+                                    precio = 99;
+                                    listaDeCuentasPagadas[listaDeCuentasPagadas.length] = precio;
+                                }
+
+    if(youtube){
+        precio = 119;
         listaDeCuentasPagadas[listaDeCuentasPagadas.length] = precio;
+    }   
+        if(hboMax){
+            precio = 104;
+            listaDeCuentasPagadas[listaDeCuentasPagadas.length] = precio;
+        }        
+            if(disney){
+                precio = 159;
+                listaDeCuentasPagadas[listaDeCuentasPagadas.length] = precio;
+            }    
+                if(primeVideo){
+                    precio = 99;
+                    listaDeCuentasPagadas[listaDeCuentasPagadas.length] = precio;
+                }            
+                    if(paramount){
+                        precio = 79;
+                        listaDeCuentasPagadas[listaDeCuentasPagadas.length] = precio;
+                    }
+                        if(blimTv){
+                            precio = 109;
+                            listaDeCuentasPagadas[listaDeCuentasPagadas.length] = precio;
+                        }                        
+                            if(claroVideo){
+                                precio = 69;
+                                listaDeCuentasPagadas[listaDeCuentasPagadas.length] = precio;
+                            }
+    let total = 0;
+    for (let i = 0; i < listaDeCuentasPagadas.length; i++) {    
+        total += listaDeCuentasPagadas[i];
     }
-    if(precio3){
-        precio = 266;
-        listaDeCuentasPagadas[listaDeCuentasPagadas.length] = precio;
-    }
-
-
-
-
-
-
-
-
-
-
+    return total;
 }
+console.log(CALCULAR());
