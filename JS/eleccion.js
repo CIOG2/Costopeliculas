b04c03a258bb4a99dfcd1f0e7ae225f256ddd180
Carlos Iván Ochoa Gómez <81680537+CIOG2@users.noreply.github.com>
2021-08-10 19:58:53 -0600

    Agrege los ID y las funciones que selecional las plataformas

--- JS/eleccion.js
+++ JS/eleccion.js
@@ -1,18 +1,19 @@
 const listaDeCuentasPagadas = [];
-let nexflix = false, precio = 0;
+let nexflix = false, youtube = false, hboMax = false, disney = false, primeVideo = false, paramount = false, hulu = false, appleTv = false, plutoTv = false, blimTv = false, crunchyroll = false, claroVideo = false; 
+let precio = 0;
 
-function done() {
-    let nexflixDone = document.getElementById('nexflix');
+function markedNexflix() {
+    let nexflixmarked = document.getElementById('nexflix');
     
     if (nexflix) {
-        document.getElementById('nexflix').classList.remove('done');
+        document.getElementById('nexflix').classList.remove('marked');
         document.getElementById('nexflixRES1').checked = false;
         document.getElementById('nexflixRES2').checked = false;
         document.getElementById('nexflixRES3').checked = false;
         
         nexflix = false;
     } else {
-        document.getElementById('nexflix').classList.add('done');
+        document.getElementById('nexflix').classList.add('marked');
 
         setTimeout(() => {
             document.getElementById('nexflixPlanes').classList.add('display'); 
@@ -24,8 +25,126 @@ function done() {
 
 
 
-function removeDisplay(id) {
-        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function markedYoutube() {
+    if(youtube){
+        document.getElementById('youtube').classList.remove('marked');
+        youtube = false;
+    } else {
+        document.getElementById('youtube').classList.add('marked');
+        youtube = true;
+    } 
+}
+function markedHBO(plataforma) {
+    if(hboMax){
+        document.getElementById(plataforma).classList.remove('marked');
+        hboMax = false;
+    } else {
+        document.getElementById(plataforma).classList.add('marked');
+        hboMax = true;
+    } 
+}
+function markedDisney(plataforma) {
+    if(disney){
+        document.getElementById(plataforma).classList.remove('marked');
+        disney = false;
+    } else {
+        document.getElementById(plataforma).classList.add('marked');
+        disney = true;
+    } 
+}
+function markedPrimeVideo(plataforma) {
+    if(primeVideo){
+        document.getElementById(plataforma).classList.remove('marked');
+        primeVideo = false;
+    } else {
+        document.getElementById(plataforma).classList.add('marked');
+        primeVideo = true;
+    } 
+}
+function markedParamount(plataforma) {
+    if(paramount){
+        document.getElementById(plataforma).classList.remove('marked');
+        paramount = false;
+    } else {
+        document.getElementById(plataforma).classList.add('marked');
+        paramount = true;
+    } 
+}
+function markedAppleTv(plataforma) {
+    if(appleTv){
+        document.getElementById(plataforma).classList.remove('marked');
+        appleTv = false;
+    } else {
+        document.getElementById(plataforma).classList.add('marked');
+        appleTv = true;
+    } 
+}
+function markedPlutoTv(plataforma) {
+    if(plutoTv){
+        document.getElementById(plataforma).classList.remove('marked');
+        plutoTv = false;
+    } else {
+        document.getElementById(plataforma).classList.add('marked');
+        plutoTv = true;
+    } 
+}
+function markedBlimTv(plataforma) {
+    if(blimTv){
+        document.getElementById(plataforma).classList.remove('marked');
+        blimTv = false;
+    } else {
+        document.getElementById(plataforma).classList.add('marked');
+        blimTv = true;
+    } 
+}
+function markedClaroVideo(plataforma) {
+    if(claroVideo){
+        document.getElementById(plataforma).classList.remove('marked');
+        claroVideo = false;
+    } else {
+        document.getElementById(plataforma).classList.add('marked');
+        claroVideo = true;
+    } 
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function removeDisplay(id) {        
     setTimeout(() => {
         document.getElementById(id).classList.remove('display');  
     }, 200);
